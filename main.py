1250e0251aa796e24792a6ef78019008236f9ac0
Will S <92five@gmail.com>
2019-03-01 23:21:37 -0700

    pep8 and move old files

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+"""
+main api for pi space camera
+"""
 import time
-import threading
+
 import recorder
 
+
 def main():
+    """ main loop for space camera api"""
     r = recorder.Recorder()
     r.start()
     time.sleep(120)
     r.stop()
 
+
 if __name__ == '__main__':
     main()
-
--- memoryManager.py
+++ memoryManager.py
@@ -1,23 +1,35 @@
-import shutil
-import time
-import os
-import threading
+"""
+MemoryManager class
+Designed to ensure disk does not fill up
+by deleteing oldest recording file if a min free disk threshold is reached
+"""
 
-from options import R_INTERVAL, R_FILE_DIR, MIN_FREE_MEM
+import os
+import shutil
+import threading
+import time
+
+from options import MIN_FREE_MEM, R_FILE_DIR, R_INTERVAL
+
 
 class MemoryManager:
+    "memoryManager class ensures disk does not fill up"
+
     def __init__(self):
         self._d = threading.Event()
         self._T = None
 
     def start(self):
+        "starts new thread to monitor free disk"
         if self._T:
             return False
         self._T = threading.Thread(target=self._checkMem)
         self._T.start()
+
         return True
 
     def stop(self):
+        "signals MemoryManager thread to stop"
         if self._T:
             self._d.set()
             self._T.join(R_INTERVAL)
@@ -26,13 +38,14 @@ class MemoryManager:
             self._T = None
             self._d.clear()
             print("memManager stoped")
+
         return True
 
     def _checkMem(self):
         while not self._d.is_set():
-            while shutil.disk_usage(R_FILE_DIR).free <= MIN_FREE_MEM and not self._d.is_set():
+            while shutil.disk_usage(
+                    R_FILE_DIR).free <= MIN_FREE_MEM and not self._d.is_set():
                 oldest = min(os.listdir(R_FILE_DIR), key=lambda x: int(x[:-5]))
-                os.remove(R_FILE_DIR+"/"+oldest)
-                print("deleting "+oldest)
-            time.sleep(R_INTERVAL/2)
-
+                os.remove(R_FILE_DIR + "/" + oldest)
+                print("deleting " + oldest)
+            time.sleep(R_INTERVAL / 2)
