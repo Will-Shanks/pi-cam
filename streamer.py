3d02f636906842b774f8538ef299699c4d074a7a
Will S <92five@gmail.com>
2019-03-03 17:01:11 -0700

    working on streaming

--- streamer.py
+++ streamer.py
@@ -8,7 +8,7 @@ import picamera
 from options import FPS, IP, R_RES, S_PORT, S_RES
 
 
-class Recorder:
+class Streamer:
     "Streams video if someone is listening"
 
     def __init__(self):
@@ -16,14 +16,14 @@ class Recorder:
         self._sock.bind((IP, S_PORT))
         self._con = None
         self._res = S_RES
-        self._T = threading.Thread(target=self._stream)
+        self._T = None
         self._d = threading.Event()
-        self._u = threading.Event()
 
     def start(self):
         "Start streamer thead if not already running"
-        if self._T.is_alive():
+        if self._T:
             return False
+        self._T = threading.Thread(target=self._listen)
         self._T.start()
         return True
 
@@ -36,17 +36,18 @@ class Recorder:
                 return False
             self._sock.close()
             self._d.clear()
+            self._T = None
         return True
 
-    def set_res(self, res):
-        "update resolution and notify streamer thread"
-        self._res = res
-        self._u.set()
-
     def _listen(self):
-        self._sock.listen(0)
-        self._con = self._sock.accept()[0].makefile('wb')
-        self._stream()
+        self._sock.settimeout(10)
+        while not self._d.is_set():
+            try:
+                self._sock.listen(0)
+                self._con = self._sock.accept()[0].makefile('wb')
+                self._stream()
+            except socket.timeout:
+                pass
 
     def _stream(self):
         try:
@@ -60,17 +61,8 @@ class Recorder:
                     format='h264',
                     resize=self._res)
                 while not self._d.is_set():
-                    if self._u.is_set():
-                        self._u.clear()
-                        camera.stop_recording(splitter_port=2)
-                        camera.start_recording(
-                            self._con,
-                            splitter_port=2,
-                            format='h264',
-                            resize=self._res)
                     camera.wait_recording(5)
                 camera.stop_recording(splitter_port=2)
         finally:
             self._con.close()
-            if not self._d.is_set():
-                self._listen()
+            self._con = None
