1a9fb3aa30e1918c25b616d5bbe2bc6ec5bb43c4
Will S <92five@gmail.com>
2019-03-03 17:01:11 -0700

    working on streaming

--- options.py
+++ options.py
@@ -15,3 +15,10 @@ MIN_FREE_MEM = int(2e9)
 S_PORT = 8001  # TCP TODO: use UDP
 S_RES = (640, 480)
 S_FPS = 5
+
+# Downloader options
+D_PORT = 8002
+
+# OpCode definitions
+ops = {}
+ops['D_REQ'] = 1
--- streamer.py
+++ streamer.py
@@ -8,7 +8,7 @@ import picamera
 from options import FPS, IP, R_RES, S_PORT, S_RES
 
 
-class Recorder:
+class Streamer:
     "Streams video if someone is listening"
 
     def __init__(self):
@@ -16,9 +16,8 @@ class Recorder:
         self._sock.bind((IP, S_PORT))
         self._con = None
         self._res = S_RES
-        self._T = threading.Thread(target=self._stream)
+        self._T = threading.Thread(target=self._listen)
         self._d = threading.Event()
-        self._u = threading.Event()
 
     def start(self):
         "Start streamer thead if not already running"
@@ -38,15 +37,12 @@ class Recorder:
             self._d.clear()
         return True
 
-    def set_res(self, res):
-        "update resolution and notify streamer thread"
-        self._res = res
-        self._u.set()
-
     def _listen(self):
-        self._sock.listen(0)
-        self._con = self._sock.accept()[0].makefile('wb')
-        self._stream()
+        self._sock.settimeout(10)
+        while not self._d.is_set():
+            self._sock.listen(0)
+            self._con = self._sock.accept()[0].makefile('wb')
+            self._stream()
 
     def _stream(self):
         try:
@@ -60,17 +56,10 @@ class Recorder:
                     format='h264',
                     resize=self._res)
                 while not self._d.is_set():
-                    if self._u.is_set():
-                        self._u.clear()
-                        camera.stop_recording(splitter_port=2)
-                        camera.start_recording(
-                            self._con,
-                            splitter_port=2,
-                            format='h264',
-                            resize=self._res)
                     camera.wait_recording(5)
                 camera.stop_recording(splitter_port=2)
         finally:
             self._con.close()
+            self._con = None
             if not self._d.is_set():
                 self._listen()
