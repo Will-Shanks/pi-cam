150503f600041ea4b839b4042ba1d9bc47413112
Will S <92five@gmail.com>
2019-03-21 02:29:19 -0600

    ready for hardware testing

--- led.py
+++ led.py
@@ -9,14 +9,15 @@ pins = [12,32,33,35]
 GPIO.setmode(GPIO.BOARD)
 try:
     for i in pins:
-        print(i)
+        print("gpio pin " + str(i))
         GPIO.setup(i, GPIO.OUT)
-        p = GPIO.PWM(i,50)
-        p.start(33)
-        time.sleep(1)
-        p.ChangeDutyCycle(66)
-        time.sleep(1)
-        p.ChangeDutyCycle(100)
+        p = GPIO.PWM(i,100)
+        p.start(0)
+        for i in range(101):
+            time.sleep(0.01)
+            p.ChangeDutyCycle(i)
+            if(i%10 == 0):
+                print("\t power: "+str(i))
         p.stop()
 finally:
     GPIO.cleanup()
