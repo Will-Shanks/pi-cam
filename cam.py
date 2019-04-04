2bea5a25ebc032b412d907df4dc751d20dbe3bb1
Will S <92five@gmail.com>
2019-04-04 12:29:49 -0600

    implemented streaming and recording at the same time

--- cam.py
+++ cam.py
@@ -11,6 +11,56 @@ from memoryManager import MemoryManager
 from options import FPS, IP, R_FILE_DIR, R_INTERVAL, R_RES, S_PORT, S_RES
 
 
+class Streamer:
+    "writes to stream if connection exists"
+
+    def __init__(self):
+        print("streamer started")
+        self._sock = socket.socket()
+        self._sock.bind((IP, S_PORT))
+        self._con = None
+        self._T = threading.Thread(target=self._listen)
+        self._T.start()
+
+    def write(self, s):
+        "writes to socket if there is a connection"
+        if self._con:
+            try:
+                self._con.write(s)
+            except:
+                self._clean_and_listen()
+
+    def close(self):
+        "clean up connections"
+        try:
+            self._con.close()
+        except:
+            pass
+        self._con = None
+        self._sock.close()
+
+    def _clean_and_listen(self):
+        print("con lost, looking for a new one")
+        try:
+            self._con.close()
+        except:
+            pass
+        self._con = None
+        self._T = threading.Thread(target=self._listen)
+        self._T.start()
+
+    def _listen(self):
+        self._sock.settimeout(10)
+        while True:
+            try:
+                self._sock.listen()
+                self._con = self._sock.accept()[0].makefile('wb')
+                break
+            except socket.timeout:
+                pass
+        print("connection found!")
+
+
 class Saver:
     "write stream into files in chunkcs of R_INTERVAL secs"
 
@@ -35,7 +85,7 @@ class Saver:
             self._fh = None
             self._fn = fn
             self._fh = open(R_FILE_DIR + "/" + fn + ".h264", 'wb')
-            print("opening file: %s.h264" %fn)
+            print("opening file: %s.h264" % fn)
         self._fh.write(s)
         # always writes all of s so return len(s)
         return len(s)
@@ -54,11 +104,7 @@ class Camera:
     "Object for recording and streaming video in a background thread"
 
     def __init__(self):
-        self._sock = socket.socket()
-        self._sock.bind((IP, S_PORT))
-        self._con = None
-        self._c = threading.Event()
-        self._lT = None
+        self._con = Streamer()
         self._S = Saver()
         self._T = None
         self._M = MemoryManager()
@@ -67,14 +113,12 @@ class Camera:
 
     def start(self):
         "Starts recording video and listening for streaming client"
-        print("starting cam")
         if self._T is not None and self._T.is_alive():
             return False
+        print("starting cam")
         self._M.start()
         self._T = threading.Thread(target=self._record)
         self._T.start()
-        self._lT = threading.Thread(target=self._listen)
-        self._lT.start()
         return True
 
     def stop(self):
@@ -84,10 +128,9 @@ class Camera:
             self._d.set()  # signal for thread to shutdown
             self._T.join(20)  # wait 20 sec, or till thread shuts down
             # check if thread shutdown
-            if self._T.is_alive() or self._lT.is_alive():
+            if self._T.is_alive():
                 return False
         self._d.clear()
-        self._c.clear()
         self._M.stop()
         return True
 
@@ -97,61 +140,14 @@ class Camera:
             c.framerate = FPS
             # Start recording to file
             c.start_recording(self._S, format='h264')
+            c.start_recording(
+                self._con, splitter_port=2, format='h264', resize=S_RES)
             while not self._d.is_set():
-                try:
-                    if self._c.is_set():
-                        print("starting stream")
-                        c.start_recording(
-                            self._con,
-                            splitter_port=2,
-                            format='h264',
-                            resize=S_RES)
-                        self._c.clear()
-                    c.wait_recording(5)
-                except BrokenPipeError:
-                    print("streaming client disconnected")
-                    c.stop_recording(splitter_port=2)
-                    self._con.close()
-                    self._con = None
-                    self._lT = threading.Thread(target=self._listen)
-                    self._lT.start()
-                except ConnectionResetError:
-                    print("streaming client disconnected")
-                    c.stop_recording(splitter_port=2)
-                    self._con.close()
-                    self._con = None
-                    self._lT = threading.Thread(target=self._listen)
-                    self._lT.start()
-
+                c.wait_recording(5)
             c.stop_recording()
             self._S.close()
-            try:
-                c.stop_recording(splitter_port=2)
-            except picamera.exc.PiCameraNotRecording:
-                pass
-            except BrokenPipeError:
-                pass
-        try:
+            c.stop_recording(splitter_port=2)
             self._con.close()
-        except AttributeError:
-            pass
-        self._con = None
-        self._sock.close()
-
-    def _listen(self):
-        print("listening for stream client")
-        self._sock.settimeout(10)
-        while not self._d.is_set():
-            # use timeout and try/except block to check _d ~ every 10 sec
-            try:
-                self._sock.listen(0)
-                print("stream client found!")
-                self._con = self._sock.accept()[0].makefile('wb')
-                self._c.set()
-                break
-            except socket.timeout:
-                print("client not found yet")
-                pass
 
 
 if __name__ == "__main__":
